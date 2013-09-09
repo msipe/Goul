2fb492965402ede9ca1e9dff9a2e72d05870dcc3
msipe <morgan.talk@gmail.com>
2013-09-09 11:02:20 -0400

    Updated the Help Command + tests to reflect the most recent functionality

--- src/DocumentUploader.UnitTests/Command/HelpCommandTest.cs
+++ src/DocumentUploader.UnitTests/Command/HelpCommandTest.cs
@@ -9,8 +9,17 @@ namespace DocumentUploader.UnitTests.Command {
   public class HelpCommandTest : BaseTestCase {
     [Test]
     public void TestExecuteAddsOneCorrectMessageToTheObserver() {
-      mObserver.Setup(o => o.AddMessages("Goul Document Uploader Version 0.1", "Commands:", "setcredentials xClient_IDx xClient_Secretx | Sets the client id and the client secret to a local .txt file", "listcredentials | lists the credentials", "clearcredentials | deletes ALL of the credential files"));
-      mCommand.Execute("message");
+      mObserver.Setup(o => o.AddMessages("Goul Document Uploader Version 0.1",
+                            "Commands:",
+                            "setcredentials xClient_IDx xClient_Secretx | Sets the client id and the client secret to a local .txt file",
+                            "listcredentials | lists the credentials",
+                            "clearcredentials | deletes ALL of the credential files",
+                            "getauthorizationurl | Retrieves a url to the Google authorization process, based on the given credentials",
+                            "authorize x AuthorizationCode x | Creates a refresh token based on the auth code retrieved from the 'getauthorizationurl",
+                            "upload xPathOfTheFileToUploadx x TitleOfTheFileOnGoogle x | Uploads a file from the given path, to the bound Google Account, with the given title "
+                            ));
+
+      mCommand.Execute("help");
     }
 
     [SetUp]
