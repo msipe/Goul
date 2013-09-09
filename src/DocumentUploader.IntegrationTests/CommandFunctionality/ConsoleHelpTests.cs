5ae78adef95b832e9f504c80056a511f97d81ed2
msipe <morgan.talk@gmail.com>
2013-09-09 16:01:30 -0400

    Updated the help IT

--- src/DocumentUploader.IntegrationTests/CommandFunctionality/ConsoleHelpTests.cs
+++ src/DocumentUploader.IntegrationTests/CommandFunctionality/ConsoleHelpTests.cs
@@ -20,7 +20,10 @@ namespace DocumentUploader.IntegrationTests.CommandFunctionality {
                                                                "Commands:",
                                                                "setcredentials xClient_IDx xClient_Secretx | Sets the client id and the client secret to a local .txt file",
                                                                "listcredentials | lists the credentials",
-                                                               "clearcredentials | deletes ALL of the credential files")));
+                                                               "clearcredentials | deletes ALL of the credential files",
+                                                               "getauthorizationurl | Retrieves a url to the Google authorization process, based on the given credentials",
+                                                               "authorize x AuthorizationCode x | Creates a refresh token based on the auth code retrieved from the 'getauthorizationurl",
+                                                               "upload xPathOfTheFileToUploadx x TitleOfTheFileOnGoogle x | Uploads a file from the given path, to the bound Google Account, with the given title ")));
     }
   }
 }
