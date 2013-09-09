130aa7f197b7f36566fb24512f15b7a730b1eec8
msipe <morgan.talk@gmail.com>
2013-09-09 11:26:26 -0400

    Removed unecessary TC

--- src/DocumentUploader.UnitTests/Models/CredentialsFileTest.cs
+++ src/DocumentUploader.UnitTests/Models/CredentialsFileTest.cs
@@ -10,13 +10,5 @@ namespace DocumentUploader.UnitTests.Models {
       Assert.Null(credentials.ClientID);
       Assert.Null(credentials.ClientSecret);
     }
-
-    [Test]
-    public void TestValuesCanBeInlaidCorrectly() {
-      var credentials = new Credentials {ClientID = "123", ClientSecret = "456"};
-
-      Assert.That(credentials.ClientID, Is.EqualTo("123"));
-      Assert.That(credentials.ClientSecret, Is.EqualTo("456"));
-    }
   }
 }
